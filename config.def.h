96ee76c7f0c0ffce55cf75758972200cd8cb7726
ZigZag35 <rayenpilote0@gmail.com>
2021-08-30 11:11:23 +0100

    added nerd glyphs and resized bar

--- config.def.h
+++ config.def.h
@@ -1,5 +1,4 @@
 /* See LICENSE file for copyright and license details. */
-
 /* constants */
 #define TERM "lxterminal"
 
@@ -13,14 +12,15 @@ static const unsigned int gappiv    = 15;       /* vert inner gap between window
 static const unsigned int gappoh    = 30;       /* horiz outer gap between windows and screen edge */
 static const unsigned int gappov    = 30;       /* vert outer gap between windows and screen edge */
 static const int smartgaps          = 0;        /* 1 means no outer gap when there is only one window */
-static const int user_bh            = 25;       /* dwm will calculate bar height, >= 1 means dwm will user_bh as bar height */
+static const int user_bh            = 28;       /* dwm will calculate bar height, >= 1 means dwm will user_bh as bar height */
 static const int showbar            = 1;        /* 0 means no bar */
 static const int topbar             = 1;        /* 0 means bottom bar */
-static const char *fonts[]          = { "monospace:size=10"};
+static const char *fonts[]          = { "Anonymice Nerd Font:size=10"};
 static const char dmenufont[]       = "monospace:size=10";
 
 /* tagging */
-static const char *tags[] = { "1", "2", "3", "4", "5", "6", "7", "8", "9" };
+static const char *tags[] = { "\uf315", "\ue795", "\ue795", "\ue795","\ue7c5", "\ue743" };
+static const char *tagsalt[] = { "1", "2", "3", "4", "5", "6" };
 
 static const Rule rules[] = {
 	/* xprop(1):
@@ -29,8 +29,7 @@ static const Rule rules[] = {
 	 */
 	/* class      instance    title       tags mask     isfloating   monitor */
 	{ "Gimp",     NULL,       NULL,       0,            1,           -1 },
-	{ "Firefox",  NULL,       NULL,       1 << 8,       0,           -1 },
-	{ "/bin/bash",  NULL,       "terminator",       0,       0,           -1 },
+	{ "Chrome",   NULL,       NULL,       1 << 8,       0,           -1 },
 };
 
 /* layout(s) */
@@ -121,6 +120,7 @@ static Key keys[] = {
 	{ MODKEY,                       XK_period, focusmon,       {.i = +1 } },
 	{ MODKEY|ShiftMask,             XK_comma,  tagmon,         {.i = -1 } },
 	{ MODKEY|ShiftMask,             XK_period, tagmon,         {.i = +1 } },
+	{ MODKEY,                       XK_n,      togglealttag,   {0} },
 	TAGKEYS(                        XK_1,                      0)
 	TAGKEYS(                        XK_2,                      1)
 	TAGKEYS(                        XK_3,                      2)
