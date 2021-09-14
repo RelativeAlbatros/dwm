d775fe50acbb304f2f7a33e4d3ed8bd9cde95b3a
ZigZag35 <rayenpilote0@gmail.com>
2021-09-14 17:52:17 +0100

    added dwmbar at launch

--- autostart.sh
+++ autostart.sh
@@ -1,16 +1,5 @@
 # set background image
 feh --bg-fill /usr/share/pixmaps/background.jpg &
 
-# welcome screen
-#initPromptScreen ()
-#{
-#	echo -n "start tmux? "
-#	read tmuxLaunch 
-#	if [[ $tmuxLaunch =~ "y" ]]; then
-#		tmux new 
-#	fi
-#}
-#
-#st -t "Welcome back..." -e initPromptScreen &
-
+dwmbar &
 exit 0
