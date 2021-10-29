95c785c262545f5455173d263c744a3c5fd6b998
ZigZag35 <rayenpilote0@gmail.com>
2021-10-29 17:16:06 +0100

    Bar is now opaque

--- config.def.h
+++ config.def.h
@@ -10,13 +10,12 @@ static const unsigned int gappih    = 5;       /* horiz inner gap between window
 static const unsigned int gappiv    = 5;       /* vert inner gap between windows */
 static const unsigned int gappoh    = 10;       /* horiz outer gap between windows and screen edge */
 static const unsigned int gappov    = 10;       /* vert outer gap between windows and screen edge */
-static const int smartgaps          = 0;        /* 1 means no outer gap when there is only one window */
+static const int smartgaps          = 1;        /* 1 means no outer gap when there is only one window */
 static const int user_bh            = 28;       /* dwm will calculate bar height, >= 1 means dwm will user_bh as bar height */
 static const int showbar            = 1;        /* 0 means no bar */
 static const int topbar             = 1;        /* 0 means bottom bar */
 static const unsigned int baralpha = 0xd0;
 static const unsigned int borderalpha = OPAQUE;
-static const char theme[] = "blue";
 static const char *fonts[]          = { "ProFontIIx Nerd Font:style=Book:size=10"}; 
 static const char dmenufont[]       = "monospace:size=10";
 #include "color-palettes/nord.h"
@@ -24,8 +23,8 @@ static const char dmenufont[]       = "monospace:size=10";
 /* alpha bar transparency */
 static const unsigned int alphas[][3]      = {
 	/*               fg      bg        border     */
-	[SchemeNorm] = { OPAQUE, baralpha, borderalpha },
-	[SchemeSel]  = { OPAQUE, baralpha, borderalpha },
+	[SchemeNorm] = { OPAQUE, OPAQUE, borderalpha },
+	[SchemeSel]  = { OPAQUE, OPAQUE, borderalpha },
 };
 
 /* tagging: dwm crashes if the number of tags do not match. */
@@ -43,11 +42,9 @@ static const Rule rules[] = {
 };
 
 /* layout(s) */
-static const float mfact     = 0.55; /* factor of master area size [0.05..0.95] */
+static const float mfact     = 0.50; /* factor of master area size [0.05..0.95] */
 static const int nmaster     = 1;    /* number of clients in master area */
 static const int resizehints = 0;    /* 1 means respect size hints in tiled resizals */
-
-/* color palette */
 #include "fibonacci.c"
 static const Layout layouts[] = {
 	/* symbol     arrange function */
@@ -55,10 +52,10 @@ static const Layout layouts[] = {
 	{ "><>",      NULL },    /* no layout function means floating behavior */
 	{ "[M]",      monocle }, /* active window takes whole space */
 
-  { "|M|",      centeredmaster  }, /* master in center and slaves on the side */
+  { "|M|",      centeredmaster  }, 
   { ">M>",      centeredfloatingmaster  }, 
 
- 	{ "[@]",      spiral }, /* fibonacci spiral */
+ 	{ "[@]",      spiral }, 
  	{ "[\\]",     dwindle }, 
 
   { "TTT",      bstack }, 
