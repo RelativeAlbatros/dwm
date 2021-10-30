7225707ff050508e578cf9280edd0c3232810f6e
ZigZag35 <rayenpilote0@gmail.com>
2021-10-30 13:00:29 +0100

    smaller gap between windows

--- config.h
+++ config.h
@@ -6,10 +6,10 @@
 /* appearance */
 static const unsigned int borderpx  = 1;        /* border pixel of windows */
 static const unsigned int snap      = 32;       /* snap pixel */
-static const unsigned int gappih    = 10;       /* horiz inner gap between windows */
-static const unsigned int gappiv    = 15;       /* vert inner gap between windows */
-static const unsigned int gappoh    = 15;       /* horiz outer gap between windows and screen edge */
-static const unsigned int gappov    = 15;       /* vert outer gap between windows and screen edge */
+static const unsigned int gappih    = 5;       /* horiz inner gap between windows */
+static const unsigned int gappiv    = 5;       /* vert inner gap between windows */
+static const unsigned int gappoh    = 10;       /* horiz outer gap between windows and screen edge */
+static const unsigned int gappov    = 10;       /* vert outer gap between windows and screen edge */
 static const int smartgaps          = 0;        /* 1 means no outer gap when there is only one window */
 static const int user_bh            = 28;       /* dwm will calculate bar height, >= 1 means dwm will user_bh as bar height */
 static const int showbar            = 1;        /* 0 means no bar */
@@ -19,11 +19,7 @@ static const unsigned int borderalpha = OPAQUE;
 static const char theme[] = "blue";
 static const char *fonts[]          = { "Anonymice Nerd Font:size=12"}; /* put your font here */
 static const char dmenufont[]       = "monospace:size=10";
-#if ( theme == blue )
-	#include "color-palettes/dark-blue.h"
-#else
-	#include "color-palettes/green-forest.h"
-#endif
+#include "color-palettes/dark-blue.h"
 
 /* alpha bar transparency */
 static const unsigned int alphas[][3]      = {
