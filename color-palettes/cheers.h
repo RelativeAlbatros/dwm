722c8e73537584bf8e1d3adbd7fcd6f4049ad3c9
ZigZag35 <rayenpilote0@gmail.com>
2021-11-04 12:52:26 +0100

    cust: theme & font modif.
    - changed the font to DroidSans for a more regular look
    - changed theme color

--- color-palettes/cheers.h
+++ color-palettes/cheers.h
@@ -17,7 +17,7 @@ static const char *colors[][3]      = {
 	/*                        fg         bg         border   */
 	[SchemeNorm]      = { col_gray3, col_gray1,  col_yellow80  },
 	[SchemeSel]       = { col_gray4, col_cyan,   col_yellow70   },
-	[SchemeStatus]    = { col_yellow60, col_gray4,   "#000000"  }, // Statusbar right {text,background,not used but cannot be empty}
+	[SchemeStatus]    = { col_yellow70, col_gray4,   "#000000"  }, // Statusbar right {text,background,not used but cannot be empty}
 	[SchemeTagsSel]   = { col_gray4, col_yellow60,   "#000000"  }, // Tagbar left selected {text,background,not used but cannot be empty}
   [SchemeTagsNorm]  = { col_yellow70, col_gray4,   "#000000"  }, // Tagbar left unselected {text,background,not used but cannot be empty}
   [SchemeInfoSel]   = { col_yellow60, col_gray4,   "#000000"  }, // infobar middle  selected {text,background,not used but cannot be empty}
