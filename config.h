475d35e4003ef53bbfbfc045218c1de8affe9059
RelativeAlbatros <daadaa.rayen@proton.me>
2025-08-23 12:03:19 +0200

    update rofi spawn keybind

--- config.h
+++ config.h
@@ -66,7 +66,7 @@ static const char *termcmd[]  = { "st", NULL };
 
 static const Key keys[] = {
 	/* modifier                     key        function        argument */
-	{ MODKEY,                       XK_p,      spawn,          {.v = dmenucmd } },
+	{ MODKEY|ShiftMask,             XK_r,      spawn,          {.v = dmenucmd } },
 	{ MODKEY|ShiftMask,             XK_Return, spawn,          {.v = termcmd } },
 	{ MODKEY,                       XK_b,      togglebar,      {0} },
 	{ MODKEY,                       XK_j,      focusstack,     {.i = +1 } },
